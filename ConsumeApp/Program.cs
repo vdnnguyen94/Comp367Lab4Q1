666319123e522c890f1679ba705869d7155c6928
Van Nguyen <vdnnguyen94@gmail.com>
2025-04-04 14:51:54 -0400

    added library string in the console app

--- ConsumeApp/Program.cs
+++ ConsumeApp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using StringUtilities;
+
+Console.WriteLine("Enter a string:");
+string input = Console.ReadLine();
+
+bool isUpper = input.StartsWithUpper();
+
+Console.WriteLine(isUpper
+    ? "✅ Starts with an uppercase letter!"
+    : "❌ Does not start with an uppercase letter.");
