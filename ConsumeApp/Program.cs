537aa762a8785058858e3fa31862377c44e2b3df
Van Nguyen <vdnnguyen94@gmail.com>
2025-04-04 15:49:31 -0400

    pipeline

--- ConsumeApp/Program.cs
+++ ConsumeApp/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using StringUtilities;
 
+
 Console.WriteLine("Enter a string:");
-string input = Console.ReadLine();
+string? input = Console.ReadLine();
+
 
 bool isUpper = input.StartsWithUpper();
 
 Console.WriteLine(isUpper
-    ? "✅ Starts with an uppercase letter!"
-    : "❌ Does not start with an uppercase letter.");
+    ? "YES Starts with an uppercase letter!"
+    : "NO  Does not start with an uppercase letter.");
