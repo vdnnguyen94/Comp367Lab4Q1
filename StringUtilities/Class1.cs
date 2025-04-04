39e9afb87ee84cdd539767f0ed2682614aa2c661
Van Nguyen <vdnnguyen94@gmail.com>
2025-04-04 14:43:22 -0400

    StringLibrary App

---
+++ StringUtilities/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace StringUtilities
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            return !string.IsNullOrEmpty(str) && char.IsUpper(str[0]);
+        }
+    }
+}
